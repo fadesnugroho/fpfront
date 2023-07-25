19faad3c4446c55407b6ea77d29f486e7caf9caf
fadesnugroho <fadesnugroho@students.amikom.ac.id>
2023-07-25 22:25:13 +0700

    oke

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -62,7 +62,7 @@ export default function ProductPage({product}){
 export async function getServerSideProps(context){
    await mongooseConnect();
    const {id} = context.query;
-   const product = await Product.findById()
+   const product = await Product.findById(id);
    return{
     props: {
         product: JSON.parse(JSON.stringify(product)),
